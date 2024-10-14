e0ec9ccaba07e255e0e3e3df97f9ae2333b84d15
ShallyC20 <carlymar204@gmail.com>
2024-10-13 21:41:34 -0400

    Role

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// use Spatie\Permission\Models\Role;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+// $role=Role::create(['name'=>'admin']);
+// $role=Role::create(['name'=>'empleado']);
+// $role=Role::create(['name'=>'cliente']);
+
 Route::get('{any}', function () {
     return view('welcome');
 })->where('any', '.*');
